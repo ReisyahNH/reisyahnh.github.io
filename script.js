3d185ab97e6c78d88e5aaa543b124c82277898d2
ReisyahNH <reisyahnayla@gmail.com>
2026-06-22 09:46:57 +0700

    Update4 script.js

--- script.js
+++ script.js
@@ -1,48 +1,72 @@
-const btn = document.getElementById("themeBtn");
+const themeBtn = document.getElementById("themeBtn");
 
-btn.onclick = () => {
+themeBtn.addEventListener("click", () => {
 
-document.body.classList.toggle("light");
+    document.body.classList.toggle("light");
 
-};
+    if(document.body.classList.contains("light")){
+        localStorage.setItem("theme","light");
+        themeBtn.innerHTML="☀️";
+    }
+    else{
+        localStorage.setItem("theme","dark");
+        themeBtn.innerHTML="🌙";
+    }
+
+});
+
+if(localStorage.getItem("theme")==="light"){
+    document.body.classList.add("light");
+    themeBtn.innerHTML="☀️";
+}
 
 function openModal(title,desc,role,link){
 
-document.getElementById("modal").style.display="block";
+    document.getElementById("modal").style.display="flex";
 
-document.getElementById("modalTitle").innerText=title;
+    document.getElementById("modalTitle").innerText=title;
 
-document.getElementById("modalDesc").innerText=desc;
+    document.getElementById("modalDesc").innerText=desc;
 
-document.getElementById("modalRole").innerText="Role : "+role;
+    document.getElementById("modalRole").innerText="Role : "+role;
 
-document.getElementById("modalLink").href=link;
-
+    document.getElementById("modalLink").href=link;
 }
 
 function closeModal(){
 
-document.getElementById("modal").style.display="none";
+    document.getElementById("modal").style.display="none";
 
 }
 
-const observer = new IntersectionObserver(entries=>{
+window.onclick = function(e){
 
-entries.forEach(entry=>{
+    const modal = document.getElementById("modal");
 
-if(entry.isIntersecting){
-
-entry.target.classList.add("show");
+    if(e.target===modal){
+        modal.style.display="none";
+    }
 
 }
 
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:0.15
 });
 
-});
+document.querySelectorAll(".fade").forEach((el)=>{
 
-document.querySelectorAll(".fade").forEach(el=>{
-
-observer.observe(el);
+    observer.observe(el);
 
 });
-
